9869bec8e9172dfbf246d1bddcf5a31dec18d737
Frank Kaumba <engeezgamer@gmail.com>
2025-04-03 09:36:18 +0200

    Add files via upload

---
+++ config.cjs
@@ -0,0 +1,40 @@
+// config.js - Optimus Prime Bot Configuration
+// Author: Frank kaumba dev
+// thanks to sir Jawad 
+const fs = require("fs");
+require("dotenv").config();
+const config = {
+
+  SESSION_ID: process.env.SESSION_ID || "EF-PRIME~nfpAlbqI#ntZPHjoSEg5rbWXzFw16MQVsutCptUVbEn2gzvgVnOs",
+  PREFIX: process.env.PREFIX || '/',
+  
+  // Message Protection
+  ANTI_DELETE: process.env.ANTI_DELETE !== undefined ? process.env.ANTI_DELETE === 'true' : true, 
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
+  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
+  STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
+  
+  // Communication Protocols
+  AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
+  AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
+  AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
+  AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
+  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+  
+  // Defensive Systems
+  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
+  REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
+  NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
+  
+  // Command Mode
+  MODE: process.env.MODE || "public",
+  
+  // Alliance Info
+  OWNER_NAME: process.env.OWNER_NAME || "Frank kaumba",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "265993702468",
+  GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
+  WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
+};
+
+module.exports = config;
